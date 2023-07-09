d26869dc279908106d8f8626abed4a61628efb43
Nate Whetsell <nathan.whetsell@gmail.com>
2023-07-09 12:04:10 -0400

    Test by parsing LilyPond regression files

--- grammar.js
+++ grammar.js
@@ -9,8 +9,9 @@ module.exports = grammar({
   ],
 
   conflicts: $ => [
-    [$.chord, $.punctuation],
-    [$._expression_component, $.assignment_lhs, $._octave_check]
+    [$._expression_component, $.assignment_lhs, $._octave_check],
+    [$.named_context],
+    [$.chord, $.punctuation]
   ],
 
   rules: Object.assign({
@@ -48,11 +49,13 @@ module.exports = grammar({
     // or argument as an “expression component”.
     _expression_component: $ => choice(
       $._assignment,
+      $._context,
       $.named_context,
       $.expression_block,
       $.parallel_music,
       $.chord,
       $.escaped_word,
+      $.quoted_identifier,
       $.symbol,
       $.property_expression,
       $.fraction,
@@ -63,7 +66,12 @@ module.exports = grammar({
       $.instrument_string_number,
       $.string,
       $._octave_check,
-      $.embedded_scheme
+      $.embedded_scheme,
+
+      // This is a hack to parse things like “jah.” and “D.C.” (note the
+      // trailing periods), which are common in lyrics and markup but aren’t
+      // property expressions.
+      seq(choice($.symbol, $.property_expression), alias('.', $.punctuation))
     ),
 
     _assignment: $ => prec(1, seq(
@@ -76,13 +84,17 @@ module.exports = grammar({
       $.property_expression
     )),
 
+    _context: $ => alias('\\context', $.escaped_word),
+
     // https://lilypond.org/doc/Documentation/notation/creating-and-referencing-contexts
-    named_context: $ => seq(
-      alias(choice('\\new', '\\context'), $.escaped_word),
-      $.symbol,
-      alias('=', $.punctuation),
-      choice($.symbol, $.string)
-    ),
+    named_context: $ => prec(1, seq(
+      choice(alias('\\new', $.escaped_word), $._context),
+      choice($.symbol, $.embedded_scheme),
+      optional(seq(
+        alias('=', $.punctuation),
+        choice($.symbol, $.string)
+      ))
+    )),
 
     expression_block: $ => seq(
       '{',
@@ -101,18 +113,30 @@ module.exports = grammar({
       repeat(choice(
         seq(alias('-', $.punctuation), alias('>', $.punctuation)),
         '>>',
+        alias(choice('\\+', '\\\\', prec(1, '\\!')), $.figured_bass_modifier),
         $._expression_component
       )),
       '>'
     ),
 
-    escaped_word: $ => token(seq('\\', choice(symbol(), /[=^]/))),
+    // An additional single character is needed to parse, for example:
+    //   \-  duration lines (https://lilypond.org/doc/Documentation/notation/graphical-notation)
+    //   \^  string bends (https://lilypond.org/doc/Documentation/notation/common-notation-for-fretted-strings#index-string-bending_002c-in-tablature-notation)
+    //   \=  phrasing slur label (https://lilypond.org/doc/Documentation/notation/expressive-marks-as-curves)
+    escaped_word: $ => prec(1, token(seq('\\', choice(symbol(), /[^\x00-\x1F\s\p{Zs}0-9"\\()\[\]\uFEFF\u2060\u200B]/)))),
+
+    quoted_identifier : $ => seq(
+      alias('\\', $.escaped_word),
+      token.immediate('"'),
+      alias(token.immediate(/[^"]+/), $.escaped_word),
+      token.immediate('"')
+    ),
 
     symbol: $ => token(symbol()),
 
     property_expression: $ => prec(1, seq(
       choice($.symbol, $.property_expression),
-      '.',
+      alias('.', $.punctuation),
       $.symbol
     )),
 
@@ -125,28 +149,32 @@ module.exports = grammar({
     unsigned_integer: $ => unsignedInteger(),
 
     punctuation: $ => choice(
-      "'",
-      ',',
-      '?',
-      '!',
-      '.',
-      '*',
-      '-',
-      '^',
-      '_',
-      '>',
-      '+',
-      '~',
-      '(',
-      ')',
-      '\\(',
-      '\\)',
-      '[',
-      ']',
-      ':',
-      '/',
-      '=',
-      '|'
+      "'",   // raise octave
+      ',',   // lower octave
+      '?',   // cautionary accidental
+      '!',   // reminder accidental, figured-bass natural
+      '.',   // property access, staccato
+      '*',   // multiplication
+      '-',   // place neutrally, figured-bass flat
+      '^',   // place above
+      '_',   // place below
+      '>',   // accent
+      '+',   // figured-bass sharp
+      '~',   // tie
+      '(',   // start slur
+      ')',   // stop slur
+      '\\(', // start phrasing slur
+      '\\)', // stop phrasing slur
+      '[',   // start manual beam
+      ']',   // stop manual beam
+      ':',   // tremolo, chord prefix
+      '/',   // fraction delimiter
+      '=',   // assignment
+      '|',   // bar check
+
+      // Prevent other punctuation from producing parsing errors.
+      '&',
+      ';'
     ),
 
     dynamic: $ => choice(
@@ -171,7 +199,7 @@ module.exports = grammar({
 
     string_fragment: $ => token.immediate(prec(1, /[^"\\]+/)),
 
-    escape_sequence: $ => token.immediate(seq('\\', /./)),
+    escape_sequence: $ => token.immediate(seq('\\', /.|[\n\r]/)),
 
     _octave_check: $ => prec(1, seq(
       $.symbol,
@@ -180,7 +208,7 @@ module.exports = grammar({
     )),
 
     embedded_scheme: $ => seq(
-      '#',
+      alias(/[#$]@?/, $.embedded_scheme_prefix),
       $.embedded_scheme_text,
     ),
 
@@ -214,15 +242,15 @@ module.exports = grammar({
 function symbol() {
   // The LilyPond lexer effectively uses /[a-zA-Z\x80-\xFF]+ for alphabetic
   // characters (see
-  // https://gitlab.com/lilypond/lilypond/-/blob/3898c185a361e76ac1bf0332f50c63d0617da669/lily/lexer.ll#L169).
+  // https://gitlab.com/lilypond/lilypond/-/blob/v2.24.1/lily/lexer.ll#L169).
   // This seems intended to match UTF-8 input, but is a bit over-inclusive (see
   // https://en.wikipedia.org/wiki/UTF-8#Codepage_layout). Instead, for
   // alphabetic characters match anything that’s not an ASCII control character,
   // a space, a digit, various punctuation marks, a ZERO WIDTH NO-BREAK
   // SPACE (U+FEFF), a WORD JOINER (U+2060), or a ZERO WIDTH SPACE (U+200B).
-  // This is from Tree-sitter’s JavaScript grammar:
+  // This is based on Tree-sitter’s JavaScript grammar:
   //   https://github.com/tree-sitter/tree-sitter-javascript/blob/fdeb68ac8d2bd5a78b943528bb68ceda3aade2eb/grammar.js#L1005)
-  const alpha = /[^\x00-\x1F\s\p{Zs}0-9:;`"'@#.,|^&<=>+\-*/\\%?!~()\[\]{}\uFEFF\u2060\u200B]+/;
+  const alpha = /[^\x00-\x1F\s\p{Zs}0-9:;`"'@#$.,|^&<=>+_\-*/\\%?!~()\[\]{}\uFEFF\u2060\u200B]+/;
   return seq(alpha, repeat(seq(/[_-]/, alpha)));
 }
 
